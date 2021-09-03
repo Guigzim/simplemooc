24dfb463b0abeba4b6cf4289d4a990c47cf5e422
Guilherme <guilhermebarrosr@gmail.com>
2021-09-03 15:20:37 -0300

    novos templates e uso do admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Course
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','start_date','created_at']
+    search_fields = ['name','slug']
+    prepopulated_fields = {'slug':['name']}
+    
+
+admin.site.register(Course, CourseAdmin)
+title = 'Administrador de Cursos'
+admin.site.site_title = title
+admin.site.site_header = title
